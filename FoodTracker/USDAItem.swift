0d148aa3832bb3e429d610567e8fac28ed318447
NIPHERY <robin.somlette@gmail.com>
2015-05-04 21:26:21 -0400

    first commit

---
+++ FoodTracker/USDAItem.swift
@@ -0,0 +1,27 @@
+//
+//  USDAItem.swift
+//  FoodTracker
+//
+//  Created by Robin Somlette on 04-05-2015.
+//  Copyright (c) 2015 Robin Somlette. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc (USDAItem)
+class USDAItem: NSManagedObject {
+
+    @NSManaged var calcium: String
+    @NSManaged var carbohydrate: String
+    @NSManaged var cholesterol: String
+    @NSManaged var dateAdded: NSDate
+    @NSManaged var energy: String
+    @NSManaged var fatTotal: String
+    @NSManaged var idValue: String
+    @NSManaged var name: String
+    @NSManaged var protein: String
+    @NSManaged var sugar: String
+    @NSManaged var vitaminC: String
+
+}
